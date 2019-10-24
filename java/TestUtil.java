6fb1880431f3cc39c8e3cd8a9fb05c14d3e11775
chris <2011578668@qq.com>
2019-10-24 11:17:12 +0800

    ç¬¬äºæ¬¡æäºt
    ¤

--- java/TestUtil.java
+++ java/TestUtil.java
@@ -1,5 +1,7 @@
 public class TestUtil {
     public static void main(String[] args) {
         System.out.println("helloWorld");
+        System.out.println("新建分支develop");
+        System.out.println("java");
     }
 }
